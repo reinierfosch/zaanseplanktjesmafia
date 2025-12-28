695a82d9da8143266706b13d181ebb659856d5e9
reinierfosch <rr.fosch@gmail.com>
2025-12-28 10:05:26 +0100

    Fix analytics script injection - move from HTML to dynamic JS injection
    - Remove analytics script from index.html (Vite doesn't replace env vars in HTML attributes)
    - Inject analytics script dynamically in main.tsx using environment variables
    - Fixes build warnings about undefined VITE_ANALYTICS_ENDPOINT and VITE_ANALYTICS_WEBSITE_ID

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { getAnalyticsEndpoint, getAnalyticsWebsiteId } from "./lib/env";
+
+// Inject analytics script if environment variables are set
+const analyticsEndpoint = getAnalyticsEndpoint();
+const analyticsWebsiteId = getAnalyticsWebsiteId();
+
+if (analyticsEndpoint && analyticsWebsiteId) {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = `${analyticsEndpoint}/umami`;
+  script.setAttribute("data-website-id", analyticsWebsiteId);
+  document.body.appendChild(script);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
